7d79248bf457c7597aebd89d30c3f3265c482762
risman94 <kamarulrisman1994@gmail.com>
2018-02-25 19:39:40 +0800

    commit

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+function isPalindrome(text) {
+  var palindrome = text.toLowerCase();
+  if (
+    palindrome
+      .split('')
+      .reverse()
+      .join('') === palindrome
+  ) {
+    return console.log(text, 'ini adalah palindrome');
+  }
+  return console.log('bukan palindrome');
+}
+
+isPalindrome('Deleveled');
+
+
+Testing this is the
